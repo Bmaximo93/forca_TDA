72c552b91bd6319c7fb94b85523f9f82361e918d
thalessantana <thalessantana5449@gmail.com>
2024-11-24 23:04:21 -0300

    fix

--- main.cpp
+++ main.cpp
@@ -19,6 +19,7 @@ typedef struct {
     char nome[MAX_NOME];
     int pontuacao;
 } Usuario;
+#define MAX_USUARIOS 100
 
 void desenharMenu();
 int validaUsuario();
@@ -58,6 +59,7 @@ int main () {
 				}
 				break;
 			case 2:
+				exibeRanking();
 				exibeRanking();
 				break;
 			case 3:
@@ -461,6 +463,12 @@ void cadastrarPalavra() {
         palavraConvertida[i] = toupper(palavra[i]);
     }
     palavraConvertida[strlen(palavra)] = '\0';
+
+	// Verificar se a palavra já existe no arquivo
+    if (palavraJaExiste(palavraConvertida, "dict.txt")) {
+        printf("Erro: A palavra \"%s\" já está cadastrada no sistema.\n", palavraConvertida);
+        exit(1);
+    }
 
 	// Verificar se a palavra já existe no arquivo
     if (palavraJaExiste(palavraConvertida, "dict.txt")) {
